05aef4c093cbee38cd4c88c0f7f3ceaa20b6221f
stayforyou <bread.s@kakao.com>
2024-05-22 10:10:30 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  // other vite config options...
+  server: {
+    fs: {
+      strict: false
+    },
+    // for SPAs
+    // https://vitejs.dev/config/#server-fallback
+    fallback: 'index.html'
+  }
+}
